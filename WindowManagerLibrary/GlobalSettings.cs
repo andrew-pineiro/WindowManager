d21b23cac9fdbf3fc715d1458863d187bdc91e10
andrew-pineiro <chillsav12345@gmail.com>
2024-10-13 11:43:32 -0400

    1.0.3 - forgotten file..

---
+++ WindowManagerLibrary/GlobalSettings.cs
@@ -0,0 +1,7 @@
+namespace WindowManagerLibrary;
+
+public static class GlobalSettings {
+    public static string LogFilePath { get; private set; } = $"C:\\Users\\Chill\\Repositories\\WindowManager\\.Logs";
+    public static int PrimaryDisplay { get; set; } = 1;
+    public static string BuildNumber { get; set; } = "1.0.3";
+}
